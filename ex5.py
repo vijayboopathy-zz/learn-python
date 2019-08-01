a598346d0405458d013f4e810fbd1c42f56aafe7
Vijayboopathy.E <vijayboopathy.e@gmail.com>
2019-08-01 10:54:49 +0530

    ex4 to ex8

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Vijayboopatny Elangovan'
+my_age = 26
+my_height = 74
+my_weight = 75
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth}.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
